6c1acd8fee486ae64ae602777f82da04322034a7
Shraddhasaini <shraddhasaini99@gmail.com>
2020-03-29 11:51:10 +0530

    importing in views.py

--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
 # Create your views here.
